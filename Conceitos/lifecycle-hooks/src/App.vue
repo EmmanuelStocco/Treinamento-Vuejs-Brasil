755a10836c7317ab21064c6c9328ff20ae424d52
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-03-01 12:35:08 -0300

    Add lifecycle-hooks  module

---
+++ Conceitos/lifecycle-hooks/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1> Lifecycle Hooks</h1>
+</template>
+
+<script>
+export default {
+    setup () {
+        onMounted(() => { //versão composition API
+            console.log('Foi montado composition API')
+        })
+    }
+}
+
+    // export default {
+    //     mounted() {
+    //         console.log('Foi montado options APIO')
+    //     }
+    // }
+</script>
